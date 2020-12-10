0df235875deaba3929d6e54f906bab686096fb21
Luke <lukeconaboy@gmail.com>
2020-12-10 11:55:45 +0000

    Updated Lagrangian calculation

--- lagrangian.py
+++ lagrangian.py
@@ -3,40 +3,81 @@ import numpy as np
 def write_namelist(ii, ex, lma):
     """Writes a partial namelist for use with cubic-mask"""
 
-    with open('zoom.param', 'w') as f:
+    with open('zoom4096.param', 'w') as f:
         f.write('&mask_params\n')
         f.write('levelmin = 8 \n')
         f.write('levelmax = {0:d}\n'.format(lma))
         f.write('ix = {0:d}\n'.format(ii[0]))
         f.write('iy = {0:d}\n'.format(ii[1]))
         f.write('iz = {0:d}\n'.format(ii[2]))
-        f.write('nx = {0:d}\n'.format(ex))
-        f.write('ny = {0:d}\n'.format(ex))
-        f.write('nz = {0:d}\n'.format(ex))
+        f.write('nx = {0:d}\n'.format(ex[0]))
+        f.write('ny = {0:d}\n'.format(ex[1]))
+        f.write('nz = {0:d}\n'.format(ex[2]))
         f.write('npad = 4\n')
-        f.write("path = '' \n")
+        f.write("path = '/store/erebos/spilipenko/ramses_test/fields/' \n")
         f.write('/\n')
 
 
-def run_refmask(cen, rad):
+def run_refmask(cen=None, rad=None, region=True):
     import subprocess
+    
+    inf = '/snap7/scratch/dp004/dc-cona1/lg/runs/oldest/output_00011'
+    ini = '/snap7/scratch/dp004/dc-cona1/lg/runs/oldest/output_00001'
+    
+    # Run based on the region, or list of particle IDs
+    if region:
+        exe = '/cosma/home/dp004/dc-cona1/music/tools/ramses/get_music_refmask'
+        rad = str(rad)
+        xc = str(cen[0])
+        yc = str(cen[1])
+        zc = str(cen[2])
+        subprocess.run([exe, '-inf', inf, '-ini', ini, '-xc', xc, '-yc', yc,
+                        '-zc', zc, '-rad', rad])
+    else:
+        exe = '/cosma/home/dp004/dc-cona1/scripts/lg/get_partid_refmask'
+        subprocess.run([exe, '-inf', inf, '-ini', ini])
 
-    exe = ''
-    inf = ''
-    ini = ''
-    rad = str(rad)
-    xc = str(cen[0])
-    yc = str(cen[1])
-    zc = str(cen[2])
 
-    subprocess.run([exe, '-inf', inf, '-ini', ini, '-xc', xc, '-yc', yc,
-                    '-zc', zc, '-rad', rad])
+def update_lims(old_lim):
+    """Checks tmp_lim against new limits from the refmask run, and updates
+    if they've changed."""
+    
+    dat = np.loadtxt('./music_region_file.txt')
+    new_lim = np.zeros((3, 2))
+
+    for i in range(3):
+        # tmp_lim[i, :] = ([dat[:, i].min(), dat[:, i].max()])
+        dmin = dat[:, i].min()
+        dmax = dat[:, i].max()
+
+        # print('dminmax')
+        # print(dmin)
+        # print(dmax)
+
+        if dmin < old_lim[i, 0]:
+            new_lim[i, 0] = dmin
+        else:
+            new_lim[i, 0] = old_lim[i, 0]
+            
+        if dmax > old_lim[i, 1]:
+            new_lim[i, 1] = dmax
+        else:
+            new_lim[i, 1] = old_lim[i, 1]
+
+    return new_lim
+
+def dilate_region(lim):
+    # Dilate region by 2% on each edge to account for padding
+    lim = np.array([lim[:, 0] * 0.98,
+                    lim[:, 1] * 1.02]).T
+
+    return lim
 
 # Highest level of refinement
-lma = 11
+lma = 12
 
 # Max refinement region size (fine grid cells)
-max_ref = 220
+max_ref = 256
 
 # Factor to expand region around centres
 fac = 1.5 # 3.0
@@ -49,6 +90,8 @@ fac = 1.5 # 3.0
 x = np.array([[47823, 52603, 47954],
               [50983, 49567, 45720]], dtype=float)  # kpc/h
 
+r = np.array([198.9, 143.0], dtype=float)
+
 # Masses of haloes
 m = np.array([1.8536e12, 7.044e11])  # Msol/h
 M = m.sum()
@@ -58,6 +101,10 @@ ub = 100000.0  # kpc/h
 
 # Factor to convert from code units to cells
 uc = 2. ** lma
+
+# Convert to code units
+xx = x / ub
+rr = r / ub
 
 # Convert x
 x /= ub
@@ -111,22 +158,60 @@ ii = cen - (ex // 2)
 print('---- corners (fine cells):         ', ii)
 print('---- running get_music_refmask')
 
+
+
 # Run the music refinement mask code to find which particles are in
 # the Lagrangian region
-run_refmask(cen/uc, dif/uc)
+# run_refmask(cen/uc, dif/uc)
 
-# This file contains the coordinates of which will end up insde the
-# specified zoom region
-dat = np.loadtxt('./music_region_file.txt')
 lim = []
 ni = []
 ii = []
 
-for i in range(dat.shape[1]):
-    lim.append([dat[:, i].min(), dat[:, i].max()])
-    ni.append(int((lim[i][1] - lim[i][0]) * uc))
-    ii.append(int(lim[i][0] * uc))
+# tmp_lim = np.zeros(shape=(3, 2))
+# tmp_lim[:, 0] = 1e30   # xmin
+# tmp_lim[:, 1] = -1e30  # xmax
 
+# Instead of just choosing large values we can start off with Sergey's
+# region
+tmp_lim = np.array([[0.453125, 0.55078125],
+                    [0.3828125, 0.484375],
+                    [0.515625, 0.625]])
+
+tmp_ni = np.zeros(3)
+tmp_ii = np.zeros(3)
+
+for j in range(xx.shape[0]):
+    run_refmask(xx[j, :], rr[j] * 8.5, region=True)  # Onorbe et al (2014)
+
+    print('xx rr')
+    print(xx[j, :], rr[j])
+
+    tmp_lim = update_lims(tmp_lim)
+
+    # This file contains the coordinates of which will end up insde the
+    # specified zoom region
+        # tmp_ni[i] = int((tmp_lim[i, 1] - tmp_lim[i, 0]) * uc)
+        # tmp_ii[i] = int(tmp_lim[i, 0] * uc)
+
+    # lim.append(tmp_lim)
+    # ni.append(tmp_ni)
+    # ii.append(tmp_ii)
+
+# Now run based on list of particle IDs from AHF
+run_refmask(region=False)
+tmp_lim = update_lims(tmp_lim)
+tmp_lim = dilate_region(tmp_lim)
+
+tmp_ni = ((tmp_lim[:, 1] - tmp_lim[:, 0]) * uc).astype(int)
+tmp_ii = (tmp_lim[:, 0] * uc).astype(int)
+
+# print(tmp_lim)
+# print(tmp_ni)
+# print(tmp_ii)
+
+# print(ni)
+# print(ii)
 # with open('l.dat', 'w+') as f:
 #     for i, c in enumerate('xyz'):
 #         f.write('n{0} = {1}'.format(c, ni[i]))
@@ -135,4 +220,5 @@ for i in range(dat.shape[1]):
 print('---- writing namelist')
 
 # Take the largest of ni, but make sure it's not bigger than max_ref
-write_namelist(ii, min([max(ni), max_ref]), lma)
+# write_namelist(ii, min([max(ni), max_ref]), lma)
+write_namelist(tmp_ii, tmp_ni, lma)
